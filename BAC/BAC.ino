90c8600c6e609f60878bdd94432fb5e91aef02ae
gchai <g@bechai.com>
2015-11-15 11:53:57 -0500

    Progress

--- BAC/BAC.ino
+++ BAC/BAC.ino
@@ -1,30 +1,5 @@
-#include <rgb_lcd.h>
-
-/*
-  Hello World.ino
-  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
-
-  Author:Loovee
-  2013-9-18
-
-  Grove - Serial LCD RGB Backlight demo.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 #include <Wire.h>
+#include "rgb_lcd.h"
 
 rgb_lcd lcd;
 
@@ -32,9 +7,16 @@ const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 const int analogPin = 0;
+int diagpin = 13;
+int state = LOW;      // the current state of the output pin
+int reading;           // the current reading from the input pin
+int previous = HIGH;    // the previous reading from the input pin
+long time = 0;         // the last time the output pin was toggled
+long debounce = 200;   // the debounce time, increase if the output flickers
 
 void setup() 
 {
+  pinMode(diagpin, OUTPUT);
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
     
@@ -53,23 +35,42 @@ void loop()
     // (note: line 1 is the second row, since counting begins with 0):
     lcd.setCursor(0, 1);
     int sensorReading = analogRead(analogPin);
-    if (sensorReading > 750){
-    lcd.clear();
-    unsigned int ppm = map(sensorReading, 750, 1024, 200 ,800);
-    //float BAC = ppm/2600;
-    //int BAC = ppm;
-    //int BAC = ppm*100;
-    //unsigned int BAC=temp/26;
-    // print the number of seconds since reset:
-    //lcd.print(".");
-    //lcd.setCursor(1,0);
-    lcd.print(ppm*100/26);}
-    //lcd.print(BAC);}
-    else{
-      lcd.clear();
-      lcd.print(
-        "BAC is too low");
-        //sensorReading);
+    if (reading == HIGH && previous == LOW && millis() - time > debounce) 
+      {
+        if (state == HIGH)
+          state = LOW;
+        else
+          state = HIGH;
+    
+        time = millis();    
+      }
+    while (state="LOW")
+      {
+        if (sensorReading > 750){
+        lcd.clear();
+        unsigned int ppm = map(sensorReading, 750, 1024, 200 ,800);
+        unsigned int bac = map (ppm, 200,800,10,50);
+        //float BAC = ppm/2600;
+        //int BAC = ppm;
+        unsigned int BAC = ppm*100;
+        unsigned int temp=BAC/26;
+        lcd.setCursor(0, 0);
+        lcd.print("Your BAC is:");
+        lcd.setCursor(0,1);
+        lcd.print(".");
+        lcd.setCursor(1,1);
+        lcd.print(temp);}
+        //lcd.print(BAC);}
+        else{
+          lcd.clear();
+          lcd.print(
+            "BAC is too low");
+          lcd.setCursor(0,1);
+          lcd.print("Drink some more!");
+          
+            //sensorReading);
+        
+        }
     }
 
     delay(100);
